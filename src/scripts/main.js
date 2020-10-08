a1f08bd145a51935db8bd6b48171b52fea6e5834
Dillon Draper <dillonbdraper@gmail.com>
2020-10-08 09:35:15 -0500

    Created generator functions for all 6 crops and imported/tested in main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,20 @@
 import { createPlan } from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js"
+import { createPotato } from "./seeds/potato.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
 
 const yearlyPlan = createPlan()
-console.log(yearlyPlan)
+
+const bean = createSoybean()
+const potato = createPotato()
+const asparagus = createAsparagus()
+const sunflower = createSunflower()
+const wheat = createWheat()
+const corn = createCorn()
+
+const cropArray = [bean, potato, asparagus, sunflower, wheat, corn]
+
+console.log(cropArray)
