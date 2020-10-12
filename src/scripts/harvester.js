f3824d5d148c1671579e61c1921e49b200fb0418
Dillon Draper <dillonbdraper@gmail.com>
2020-10-12 11:31:30 -0500

    Refactored code to pass tests by handling corn arrays in field.js rather than harvester.js

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,16 +1,15 @@
 export const harvestPlants = (plants) => {
     let harvest = [];
     for (const crop of plants) {
-        if (Array.isArray(crop)) {
-            const cornArray = crop[0]
-            for (let i = 0; i < cornArray.output; i++) {
-                harvest.push(crop[0])
+        if (crop.type === "Corn") {
+            for (let i = 0; i < crop.output; i += 2) {
+                harvest.push(crop)
             }
-        }
-
+        } else {
             for (let i = 0; i < crop.output; i++) {
                 harvest.push(crop)
             }
         }
+    }
     return harvest
 }
