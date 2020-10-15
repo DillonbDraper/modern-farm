f60693d2b7e4b59aaf78d79c3791a880a006f707
Dillon Draper <dillonbdraper@gmail.com>
2020-10-15 10:31:06 -0500

    Added icon property to each seed object, added icon directory that it links to.  Made them  display in DOM instead of text

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -2,7 +2,7 @@ export const Catalog = (harvestedFood) => {
     let htmlTarget = document.querySelector(".container")
     let id = 1
     for (const food of harvestedFood) {
-        htmlTarget.innerHTML += `<section class="plant" id="${id}">${food.type}</section>`
+        htmlTarget.innerHTML += `<section class="plant" id="${id}"><img src=${food.icon}></section>`
         id++
     }
 }
--- src/scripts/seeds/corn.js
+++ src/scripts/seeds/corn.js
@@ -3,12 +3,14 @@ export const createCorn = () => {
         {
             type: "Corn",
             height: 180,
-            output: 6
+            output: 6,
+            icon: `/icons/corn.png`
         },
         {
             type: "Corn",
             height: 180,
-            output: 6
+            output: 6,
+            icon: `/icons/corn.png`
         }
     ]
 }
